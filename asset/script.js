9f51f640ea71e67a5a2e17dc972d9970b1703196
Jeffery <jefferywojo1998@gmail.com>
2021-09-08 01:11:42 -0500

    changing the text to the button on my att in my historyEl

--- asset/script.js
+++ asset/script.js
@@ -98,7 +98,7 @@ function initPage() {
         historyEl.innerHTML = "";
         for (let i=0; i<searchHistory.length; i++) {
             const historyItem = document.createElement("input");
-            historyItem.setAttribute("type","text");
+            historyItem.setAttribute("type","button");
             historyItem.setAttribute("readonly",true);
             historyItem.setAttribute("class", "form-control d-block bg-white");
             historyItem.setAttribute("value", searchHistory[i]);
